933c5bae1a8a6c0c081b66a0ae420a3827c1041c
Fish-in-a-Barrel <aaron.m.johnson@gmail.com>
2023-12-06 11:56:59 -0600

    Sending display init commands in one block

--- NixieDriverTester.X/oled.c
+++ NixieDriverTester.X/oled.c
@@ -62,50 +62,22 @@ void SetupDisplay(void)
         0x00,           // Start command sequence
         0xAE,           // Display off (10.1.12)
         0xD5, 0x80,     // Set display clock divider (10.1.16)
-        0xA8, 0x1F      // Set multiplex ratio (10.1.11)
-    };
-    
-    I2C_Write(I2C_ADDRESS, init1, sizeof(init1));
-    
-    const uint8_t init2[] =
-    {
-        0x00,           // Start command sequence
+        0xA8, 0x1F,     // Set multiplex ratio (10.1.11)
         0xD3, 0x00,     // Set display offset: 0 (10.1.15)
         0x40 | 0x00,    // Set display start line (10.1.6)
-        0x8D, 0x14      // Set charge pump (2.1 of the application note)
-    };
-    
-    I2C_Write(I2C_ADDRESS, init2, sizeof(init2));
-    
-    const uint8_t init3[] =
-    {
-        0x00,           // Start command sequence
+        0x8D, 0x14,     // Set charge pump (2.1 of the application note)
         0x20, 0x00,     // Set memory mode: horizontal addressing (10.1.3)
         0xA0 | 0x00,    // Set segment remapping: 0 (10.1.8)
-        0xC8            // Set COM output scan direction: normal (10.1.14)
-    };
-    
-    I2C_Write(I2C_ADDRESS, init3, sizeof(init3));
-    
-    const uint8_t init4[] =
-    {
-        0x00,           // Start command sequence
+        0xC8,           // Set COM output scan direction: normal (10.1.14)
         0xDA, 0x02,     // Set pin configuration: normal (10.1.18)
-        0x81 | 0xA0     // Set contrast: 0 (10.1.7)
-    };
-    
-    I2C_Write(I2C_ADDRESS, init4, sizeof(init4));
-    
-    const uint8_t init5[] =
-    {
-        0x00,           // Start command sequence
+        0x81 | 0xA0,    // Set contrast: 0 (10.1.7)
         0xA4,           // Render w/ RAM contents (10.1.9)
         0xA6,           // Set pixel state: normal (10.1.10)
         0x2E,           // Deactivate scrolling (10.2.3)
         0xAF            // Display on
     };
     
-    I2C_Write(I2C_ADDRESS, init5, sizeof(init5));
+    I2C_Write(I2C_ADDRESS, init1, sizeof(init1));
     
     Clear();
 }
