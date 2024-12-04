70401057b100627b013fd8fc691ab0467b379a06
Fish-in-a-Barrel <aaron.m.johnson@gmail.com>
2024-12-03 21:20:59 -0600

    Simplify padding bits definitions

--- ClockController.X/rtc.h
+++ ClockController.X/rtc.h
@@ -15,32 +15,32 @@ struct RtcData
     // 0x00 - seconds
     uint8_t second01:4;
     uint8_t second10:3;
-    uint8_t zero0:1;
+    uint8_t :1;
     
     // 0x01 - minutes
     uint8_t minute01:4;
     uint8_t minute10:3;
-    uint8_t zero1:1;
+    uint8_t :1;
     
     // 0x02 - hours
     uint8_t hour01:4;
     uint8_t hour10:2;
     uint8_t hoursType:1; // 0 = 12, 1 = 24
-    uint8_t zero2:1;
+    uint8_t :1;
     
     // 0x3 - day
     uint8_t day:3;
-    uint8_t zero3:5;
+    uint8_t :5;
     
     // 0x4 - date
     uint8_t date01:4;
     uint8_t date10:2;
-    uint8_t zero4:2;
+    uint8_t :2;
     
     // 0x5 - month
     uint8_t month01:4;
     uint8_t month10:1;
-    uint8_t zero5:2;
+    uint8_t :2;
     uint8_t century:1;
     
     // 0x6 - year
